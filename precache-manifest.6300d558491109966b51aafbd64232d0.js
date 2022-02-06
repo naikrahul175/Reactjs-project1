949fe155a0f28bb1e69a04f57b7914005cfd12f5
naikrahul175 <naikrahul175@gmail.com>
2022-02-06 19:58:15 +0530

    Updates

---
+++ precache-manifest.6300d558491109966b51aafbd64232d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6941b96a70f6454d19c1524d27fd323",
+    "url": "/Reactjs-project1/index.html"
+  },
+  {
+    "revision": "c758577234b4efadf41b",
+    "url": "/Reactjs-project1/static/css/main.0815bcf4.chunk.css"
+  },
+  {
+    "revision": "6c0290f0b0d079c134b1",
+    "url": "/Reactjs-project1/static/js/2.c2202c24.chunk.js"
+  },
+  {
+    "revision": "2b083cc06c92c6722354",
+    "url": "/Reactjs-project1/static/js/3.bc44d43f.chunk.js"
+  },
+  {
+    "revision": "c758577234b4efadf41b",
+    "url": "/Reactjs-project1/static/js/main.6c98477d.chunk.js"
+  },
+  {
+    "revision": "cfd0a9c5a888a0c7906b",
+    "url": "/Reactjs-project1/static/js/runtime~main.838706f5.js"
+  }
+]);
